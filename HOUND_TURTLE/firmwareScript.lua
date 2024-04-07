674a17cb5adda6f55b9d9f37ec8fe85d84965f04
19PHOBOSS98 <37253663+19PHOBOSS98@users.noreply.github.com>
2024-04-07 18:10:45 +0800

    Made magitech hound turret

--- HOUND_TURTLE/firmwareScript.lua
+++ HOUND_TURTLE/firmwareScript.lua
@@ -40,6 +40,31 @@ local instance_configs = {
 			}
 		},
 		]]--
+		PID_SETTINGS=
+		{
+			POS = {
+				P = 4,
+				I = 0,
+				D = 5
+			},
+			ROT = {
+				X = {
+					P = 0.09,
+					I = 0.01,
+					D = 0.15
+				},
+				Y = {
+					P = 0.04,
+					I = 0.03,
+					D = 0.05
+				},
+				Z = {
+					P = 0.10,
+					I = 0.01,
+					D = 0.15
+				}
+			}
+		},
 	},
 	channels_config = {
 		DEBUG_TO_DRONE_CHANNEL = 9,
@@ -59,8 +84,62 @@ local instance_configs = {
 	rc_variables = {
 		orbit_offset = vector.new(7,5,10),
 	},
+	hound_custom_config = {
+		ALTERNATING_FIRE_SEQUENCE_COUNT = 3,
+	},
 }
 
+repulsor = peripheral.find("opencu:repulsor")
+
+repulsor.recalibrateByIdx(1)
+repulsor.setRadius(5)
+repulsor.setForce(1)
+repulsor.setVector(0,140,0)
 
 local drone = HoundTurretBase(instance_configs)
+
+function drone:getProjectileSpeed()
+	return 140
+end
+
+local laserOn = false
+function activateLaser()
+	if(not laserOn) then
+		redstone.setOutput("right",false)
+		redstone.setOutput("left",true)
+		os.sleep(0.1)
+		redstone.setOutput("left",false)
+		laserOn = true
+	end
+end
+
+function deactivateLaser()
+	if(laserOn) then
+		redstone.setOutput("left",false)
+		redstone.setOutput("right",true)
+		os.sleep(0.1)
+		redstone.setOutput("right",false)
+		laserOn = false
+	end
+end
+
+function drone:onGunsActivation()
+	repulsor.pulse(0,0,0)
+	activateLaser()
+end
+
+function drone:onGunsDeactivation()
+	deactivateLaser()
+end
+
+function drone:alternateFire(step)
+	local seq_1 = step==0
+	local seq_2 = step==1
+	local seq_3 = step==2
+	--{modem_block, redstoneIntegrator_side}
+	self:activateAllGuns({"front","front"},seq_1)
+	self:activateAllGuns({"front","left"},seq_2)
+	self:activateAllGuns({"front","top"},seq_3)
+end
+
 drone:run()
